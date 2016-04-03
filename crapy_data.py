ee7a9f3b8d54c760a92ab6467005f04f3c3bcf6c
zuoyuan <zuoyuan@mail.ustc.edu.cn>
2016-04-03 23:39:40 +0800

    All URL

--- crapy_data.py
+++ crapy_data.py
@@ -28,48 +28,50 @@ def save_url(url):
 	f.write(url + '\n')
 	f.close()
 	
-base_url = r'http://zu.fang.com/house/i2%d/'
+base_url = r'http://zu.fang.com/house/i%d%d/'
 
 hourse_dl_re = re.compile(r'<dl class="list hiddenMap rel">.+?</dl>',  re.S)
 
-fp = open('fj.txt', 'a');
 
-for i in range(1,101):
-	url = base_url % i
-	html = urllib2.urlopen(url).read()
-	html = gzip.GzipFile(fileobj=StringIO(html)).read()
-	#open('data.html','w').write(html)
-	print url
-	
-	dls = hourse_dl_re.findall(html)
-	
-	for dl in dls:
-		open('dl.html','w').write(dl)
-		T = get_dom_from_string(dl.decode('gbk','ignore'))
-		link = T.find('//p[@class="title"]/a')
-		title = link.attrib['title']
-		href =  link.attrib['href']
+fp = open('fj_%s.txt' % time.strftime('%Y%m%d'), 'w');
+
+for s in range(1,4):
+	for i in range(1,101):
+		url = base_url % (s, i)
+		html = urllib2.urlopen(url).read()
+		html = gzip.GzipFile(fileobj=StringIO(html)).read()
+		#open('data.html','w').write(html)
+		print url
 		
-		if href in visited_urls:
-			continue
-		else:
-			save_url(href)
+		dls = hourse_dl_re.findall(html)
+		
+		for dl in dls:
+			open('dl.html','w').write(dl)
+			T = get_dom_from_string(dl.decode('gbk','ignore'))
+			link = T.find('//p[@class="title"]/a')
+			title = link.attrib['title']
+			href =  link.attrib['href']
 			
-		
-		infos = dom_text_content(T.find('//p[@class="font16 mt20 bold"]'))
-		
-		addrs = dom_text_content(T.find('//p[@class="gray6 mt20"]'))
-		
-		price = T.find('//span[@class="price"]').text
+			if href in visited_urls:
+				continue
+			else:
+				save_url(href)
+				
 			
-		data = [title, href,  infos, addrs, price]
+			infos = dom_text_content(T.find('//p[@class="font16 mt20 bold"]'))
+			
+			addrs = dom_text_content(T.find('//p[@class="gray6 mt20"]'))
+			
+			price = T.find('//span[@class="price"]').text
+				
+			data = [title, href,  infos, addrs, price]
+			
+			fp.writelines('\t'.join(data).encode('utf-8','ignore') + "\n")
+			
+			#print href
+		#break
+		time.sleep(2)
 		
-		fp.writelines('\t'.join(data).encode('utf-8','ignore') + "\n")
-		
-		#print href
-	#break
-	time.sleep(2)
-	
 
 fp.close()
 
